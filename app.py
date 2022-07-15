fb2c4d970ce2273a8038808ef218a1312336dfbe
Viktoriia Nikitiuk <viktoria.nikitiuk@gmail.com>
2022-07-14 17:25:31 -0700

    Added MySQL database

--- app.py
+++ app.py
@@ -1,8 +1,25 @@
+import mysql.connector
 from flask import Flask, redirect, render_template
 
 # Configure application
 app = Flask(__name__)
 
+# Connect to database
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="todo_list",
+  password="0000",
+  database="todo_list"
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("SELECT * FROM huesta")
+
+myvar = mycursor.fetchall()
+
+print(myvar)
+
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", myvar=myvar)
